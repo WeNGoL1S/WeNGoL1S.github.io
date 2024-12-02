a17b312f041195aeb58861536b6ef6f261999b25
WeNGoL1S <tveritin101018@gmail.com>
2024-12-02 23:23:24 +0500

    tests2

--- csgo/asa.js
+++ csgo/asa.js
@@ -1,3 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"
+	xmlns:content="http://purl.org/rss/1.0/modules/content/"
+	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+	xmlns:dc="http://purl.org/dc/elements/1.1/"
+	xmlns:atom="http://www.w3.org/2005/Atom"
+	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+	xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+	
+	xmlns:georss="http://www.georss.org/georss"
+	xmlns:geo="http://www.w3.org/2003/01/geo/wgs84_pos#"
+	>
+
+<channel>
+	<title>Counter-Strike: Global Offensive</title>
+	<atom:link href="https://blog.counter-strike.net/ru/index.php/feed/?gameversion=1733162404" rel="self" type="application/rss+xml" />
+	<link>https://blog.counter-strike.net/ru/</link>
+	<description></description>
+	<lastBuildDate>Thu, 27 Apr 2023 17:56:11 +0000</lastBuildDate>
+	<language>ru-RU</language>
+	<sy:updatePeriod>
+	hourly	</sy:updatePeriod>
+	<sy:updateFrequency>
+	1	</sy:updateFrequency>
+
+
+<image>
+	<url>https://s0.wp.com/i/webclip.png</url>
+	<title>Counter-Strike: Global Offensive</title>
+	<link>https://blog.counter-strike.net/ru/</link>
+	<width>32</width>
+	<height>32</height>
+</image> 
+<cdn>/media.steampowered.com/media.st.dl.eccdnx.com/</cdn><cdn>/broadcast.steampowered.com/broadcast.st.dl.eccdnx.com/</cdn><cdn>/steamcdn-a.akamaihd.net/media.st.dl.eccdnx.com/</cdn><test>/ignore me/caching test/</test><site xmlns="com-wordpress:feed-additions:1">155055202</site>	<item>
+		<title>Коллекция Anubis</title>
+		<link>https://blog.counter-strike.net/ru/index.php/2023/04/41400/</link>
+		
+		<dc:creator><![CDATA[CS:GO Team]]></dc:creator>
+		<pubDate>Tue, 25 Apr 2023 01:23:12 +0000</pubDate>
+				<category><![CDATA[Home @ru]]></category>
+		<guid isPermaLink="false">https://blog.counter-strike.net/?p=41400</guid>
+
+					<description><![CDATA[Ещё с ноября игроки украдкой выглядывали в центр и шли напролом через воды на карте Anubis. Сегодня мы представляем коллекцию Anubis, в которую входят 19 тематических раскрасок оружия для карты, недавно пополнившей список передовой. Коллекцию Anubis уже можно купить во внутриигровом магазине.]]></description>
+										<content:encoded><![CDATA[<p>Ещё с ноября игроки украдкой выглядывали в центр и шли напролом через воды на карте Anubis.</p>
+<p>Сегодня мы представляем коллекцию Anubis, в которую входят 19 тематических раскрасок оружия для карты, недавно пополнившей список передовой.</p>
+<p><img decoding="async" src="https://cdn.akamai.steamstatic.com/steamcommunity/public/images/clans/3381077/fd18c8adb36e6741b8729c4d6ffbb05cba467aa0.png" alt="" width="100%"/></p>
+<p>Коллекцию Anubis уже можно купить во внутриигровом магазине.</p>
+]]></content:encoded>
+
 		<post-id xmlns="com-wordpress:feed-additions:1">41101</post-id>	</item>
 		<item>
 		<title>Понял</title>
@@ -60,3 +108,7 @@
 <p>В ближайшие недели мы расскажем подробнее о новом процессе регионального зачёта.</p>
 <p>Поздравляем команду Outsiders! Увидимся в Париже!</p>
 ]]></content:encoded>
+
+		<post-id xmlns="com-wordpress:feed-additions:1">39433</post-id>	</item>
+	</channel>
+</rss>
