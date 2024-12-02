73bb6ec12e41294f562317ac529622eb78f515af
WeNGoL1S <tveritin101018@gmail.com>
2024-12-02 23:26:28 +0500

    test3

--- csgo/asa.js
+++ csgo/asa.js
@@ -21,7 +21,7 @@
 	hourly	</sy:updatePeriod>
 	<sy:updateFrequency>
 	1	</sy:updateFrequency>
-
+	
 
 <image>
 	<url>https://s0.wp.com/i/webclip.png</url>
@@ -45,70 +45,7 @@
 <p><img decoding="async" src="https://cdn.akamai.steamstatic.com/steamcommunity/public/images/clans/3381077/fd18c8adb36e6741b8729c4d6ffbb05cba467aa0.png" alt="" width="100%"/></p>
 <p>Коллекцию Anubis уже можно купить во внутриигровом магазине.</p>
 ]]></content:encoded>
-
-		<post-id xmlns="com-wordpress:feed-additions:1">41101</post-id>	</item>
-		<item>
-		<title>Понял</title>
-		<link>https://blog.counter-strike.net/ru/index.php/2022/12/40639/</link>
 		
-		<dc:creator><![CDATA[CS:GO Team]]></dc:creator>
-		<pubDate>Tue, 13 Dec 2099 23:57:39 +0000</pubDate>
-				<category><![CDATA[Home @ru]]></category>
-		<guid isPermaLink="false">https://blog.counter-strike.net/?p=40639</guid>
-
-					<description><![CDATA[1 Все готовы, солдаты? С 1 января вы сможете получить совершенно новую медаль за службу в 2023 году! Зарабатывайте опыт в официальных режимах игры и повышайте ранг профиля — достигнув ранга маршала (уровень 40), вы сможете обнулить его и получить (или улучшить) медаль за службу в 2023 году, которую будет видно рядом с вашим аватаром. [&#8230;]]]></description>
-										<content:encoded><![CDATA[<p><img decoding="async" src="https://cdn.akamai.steamstatic.com/apps/csgo/images/blog/service_medal_2023.png" alt="" width="100%" />1<br />
-Все готовы, солдаты? С 1 января вы сможете получить совершенно новую медаль за службу в 2026 году!</p>
-<p>Зарабатывайте опат в официальных режимах игры и повышайте ранг профиля — достигнув ранга маршала (уровень 41), вы сможете обнулить его и получить (или улучшить) медаль за службу в 2023 году, которую будет видно рядом с вашим аватаром.</p>
-<p>Также сегодняшнее обновление включает в себя две новые карты от сообщества для режима «Напарники»: Boyard и Chalice. Обе карты позволяют взглянуть на этот режим по-новому: на Boyard вас ждут динамичные сражения, а на Chalice — дуэли на дальних расстояниях.</p>
-<p>Скорее залетайте в режим «Напарники», чтобы сыграть на новых картах и заработать побольше опыта!</p>
-]]></content:encoded>
-					
-		
-		
-		<post-id xmlns="com-wordpress:feed-additions:1">40639</post-id>	</item>
-		<item>
-		<title>Анубис</title>
-		<link>https://blog.counter-strike.net/ru/index.php/2022/11/40572/</link>
-		
-		<dc:creator><![CDATA[CS:GO Team]]></dc:creator>
-		<pubDate>Fri, 18 Nov 2021 22:01:45 +0000</pubDate>
-				<category><![CDATA[Home @ru]]></category>
-		<guid isPermaLink="false">https://blog.counter-strike.net/?p=40572</guid>
-
-					<description><![CDATA[В да. обновлении преобразился список карт передовой и изменилось некоторое оружие. Для начала: мы изменили список карт передовой, на которых будет проводиться парижский мейджор в 2023 году. В карты передовой добавлена Anubis, а Dust II — удалена из списка. Обе карты по-прежнему доступны в режимах «Обычный», «Бой насмерть» и «Соревновательный». Также мы немного изменили оружие: [&#8230;]]]></description>
-										<content:encoded><![CDATA[<p>В сегодняшнем обновлении преобразился список карт передовой и изменилось некоторое оружие.</p>
-<p><img decoding="async" src="https://cdn.akamai.steamstatic.com/apps/csgo/images/blog/anubis_map.jpg" alt="" width="100%" /><br />
-Для начала: мы изменили список карт передовой, на которых будет проводиться парижский мейджор в 2023 году.</p>
-<p>В карты передовой добавлена Anubis, а Dust II — удалена из списка. Обе карты по-прежнему доступны в режимах «Обычный», «Бой насмерть» и «Соревновательный».</p>
-<p>Также мы немного изменили оружие: размер магазина AWP уменьшился до 5 (так что важен каждый выстрел!), а M4A1-S теперь наносит меньше урона издалека. </p>
-<p>Попробуйте новые изменения и расскажите, что думаете!</p>
-<p>И спасибо за англоязычный заголовок этой записи, Райан (@Schaffurr)!</p>
-]]></content:encoded>
-					
-		
-		
-		<post-id xmlns="com-wordpress:feed-additions:1">40572</post-id>	</item>
-		<item>
-		<title>Чемпионы IEM Rio Major 2022</title>
-		<link>https://blog.counter-strike.net/ru/index.php/2022/11/40399/</link>
-		
-		<dc:creator><![CDATA[CS:GO Team]]></dc:creator>
-		<pubDate>Wed, 16 Nov 2022 01:24:08 +0000</pubDate>
-				<category><![CDATA[Home @ru]]></category>
-		<guid isPermaLink="false">https://blog.counter-strike.net/?p=40399</guid>
-
-					<description><![CDATA[В течение последних двух недель Рио-де-Жанейро был центром Counter-Strike, где 24 лучшие команды мира боролись за чемпионский титул на IEM Rio Major. В конце концов осталась только одна команда: Outsiders победила Heroic на Mirage и Overpass, впервые выиграв мейджор-турнир! Чтобы отпраздновать появление новых чемпионов, сегодня мы выпускаем особую капсулу, в которой представлены наклейки с автографами [&#8230;]]]></description>
-										<content:encoded><![CDATA[<p><img decoding="async" src="https://cdn.akamai.steamstatic.com/apps/csgo/images/blog/steam_cover_champions_en.jpg" alt="" width="100%" /><br />
-В течение последних двух недель Рио-де-Жанейро был центром Counter-Strike, где 24 лучшие команды мира боролись за чемпионский титул на IEM Rio Major. В конце концов осталась только одна команда: Outsiders победила Heroic на Mirage и Overpass, впервые выиграв мейджор-турнир!</p>
-<p>Чтобы отпраздновать появление новых чемпионов, сегодня мы выпускаем особую капсулу, в которой представлены наклейки с автографами FL1T, fame, Jame, n0rb3r7 и Qikert — бумажные, с блёстками, голографические и золотые.</p>
-<h4>Новый путь к мейджорам</h4>
-<p>Начиная с парижского мейджора 2023 года мы собираемся изменить способ приглашения команд на РМР-турниры.</p>
-<p>Ранее в них принимали участие команды, приглашённые напрямую или прошедшие через открытые отборы. Что касается Парижа, то некоторые команды, не приглашённые напрямую, смогут пропустить открытые отборы: если коллектив стабильно хорошо выступает на турнирах от Valve и сторонних организаторов, то его напрямую пригласят в закрытые отборы. Там он будет соревноваться с командами, уже прошедшими открытый отбор.</p>
-<p>В ближайшие недели мы расскажем подробнее о новом процессе регионального зачёта.</p>
-<p>Поздравляем команду Outsiders! Увидимся в Париже!</p>
-]]></content:encoded>
-
 		<post-id xmlns="com-wordpress:feed-additions:1">39433</post-id>	</item>
 	</channel>
 </rss>
